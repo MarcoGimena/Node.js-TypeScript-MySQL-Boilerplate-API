73d6e8c81765f85830701c6b2933ed4a5db8f20d
Marco Gimena <marcogimena401@gmail.com>
2026-05-20 10:27:03 +0800

    fix: read database credentials from process.env

--- _helpers/db.ts
+++ _helpers/db.ts
@@ -10,7 +10,11 @@ export default db;
 initialize();
 
 async function initialize() {
-    const { host, port, user, password, database } = config.database;
+    const host = process.env.DB_HOST || config.database.host;
+    const port = Number(process.env.DB_PORT) || config.database.port;
+    const user = process.env.DB_USER || config.database.user;
+    const password = process.env.DB_PASSWORD || config.database.password;
+    const database = process.env.DB_NAME || config.database.database;  
 
     // Create DB if it doesn't exist
     const connection = await mysql.createConnection({ host, port, user, password });
